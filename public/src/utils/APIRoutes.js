6b8e36a3bdd12e1434a7ace3577f0a24841145d1
Gabriel Pabon <gabosoft.ape@gmail.com>
2023-04-12 18:55:21 -0500

    update for production server

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://45.79.39.125:4050";
+export const host = "https://anonymchat.arpadine.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
