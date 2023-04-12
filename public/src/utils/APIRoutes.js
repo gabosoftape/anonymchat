0a05c140c8c882c9622d96de0d00eb372ce60bd0
Gabriel Pabon <gabosoft.ape@gmail.com>
2023-04-12 18:14:54 -0500

    update endpoint http

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://arpadine.com:4050";
+export const host = "http://traccar.arpadine.com:4050";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
