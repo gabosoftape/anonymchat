698c6970d6a5d576449829bd28cbbbd6ac2edd62
Gabriel Pabon <gabosoft.ape@gmail.com>
2023-04-12 16:46:06 -0500

    update api routes

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:4050";
+export const host = "http://127.0.0.1:4050";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
