d146399496904e6aa5b8e815f4c971f559e5f1f5
Gabriel Pabon <gabosoft.ape@gmail.com>
2023-04-13 12:19:23 -0500

    u´pdate client socket

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -27,7 +27,7 @@ export default function Chat() {
   }, []);
   useEffect(() => {
     if (currentUser) {
-      socket.current = io(hostSocket);
+      socket.current = io.connect(hostSocket);
       socket.current.emit("add-user", currentUser._id);
     }
   }, [currentUser]);
