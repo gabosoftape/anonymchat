d95db0f499435f2ebc44e00b2323f938309a1498
Gabriel Pabon <gabosoft.ape@gmail.com>
2023-04-13 12:22:27 -0500

    update uris

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const hostSocket = "anonymchat.arpadine.com/socket";
+export const hostSocket = "https://anonymchat.arpadine.com/api";
 export const host = "https://anonymchat.arpadine.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
