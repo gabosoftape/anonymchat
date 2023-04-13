1d1f0343db2268ea70fc0b7fb0df75d365dcc384
Gabriel Pabon <gabosoft.ape@gmail.com>
2023-04-13 16:07:07 -0500

    update receve messages

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -63,7 +63,7 @@ export default function ChatContainer({ currentChat, socket }) {
         setArrivalMessage({ fromSelf: false, message: msg });
       });
     }
-  }, [arrivalMessage]);
+  }, []);
 
   useEffect(() => {
     arrivalMessage && setMessages((prev) => [...prev, arrivalMessage]);
