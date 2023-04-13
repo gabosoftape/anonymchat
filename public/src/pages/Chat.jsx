f87ed18d833f6f3bdc43bc6dc6d0c1e6b633d303
Gabriel Pabon <gabosoft.ape@gmail.com>
2023-04-13 15:03:15 -0500

    test socket connect

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -28,6 +28,7 @@ export default function Chat() {
   useEffect( () => {
     if (currentUser) {
       socket.connect();
+      console.log(socket.active);
       if(socket){
         socket.emit("add-user", currentUser._id);
       }else{
