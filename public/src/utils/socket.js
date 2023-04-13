42a866ee44dae69174fff3dac89b556d92805f07
Gabriel Pabon <gabosoft.ape@gmail.com>
2023-04-13 14:51:07 -0500

    test socket connect

--- public/src/utils/socket.js
+++ public/src/utils/socket.js
@@ -4,4 +4,8 @@ import {hostSocket} from "./APIRoutes";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? 'http://localhost:4050' : hostSocket;
 
-export const socket = io(URL);
+export const socket = io(URL,{     // note changed URL here
+    path: '/seacher',
+    autoConnect: false,
+    transports: ['websocket'],
+});
